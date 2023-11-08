f18ae6c9d1c7f6fd1be4e77573d4c2256b3236b8
Nadeesha <ashan.indrajith@dialog.lk>
2023-11-08 13:17:05 +0530

    update script

--- src/test/java/AmazonSiteTest.java
+++ src/test/java/AmazonSiteTest.java
@@ -13,6 +13,7 @@ public class AmazonSiteTest {
 
     @BeforeTest
     public void testOpenUrl() {
+        //Navigate to official Amazon site (https://www.amazon.com/)
         driver.get("https://www.amazon.com/");
         driver.manage().window().maximize();
 
@@ -21,16 +22,22 @@ public class AmazonSiteTest {
     @Test
     public void testAmazonSite() {
 
+        //Select ‘Books’ from the Category list
         Select dropDownCategory = new Select(driver.findElement(By.id("searchDropdownBox")));
         dropDownCategory.selectByVisibleText("Books");
 
+        //Select ‘Books’ from the Category list
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Automation");
         driver.findElement(By.id("nav-search-submit-button")).click();
 
+         //Select Customer Reviews as “4 Stars & Up”
         driver.findElement(By.xpath("//*[@id=\"p_72/1250221011\"]/span/a/section/i")).click();
 
+        //Select Language as “English”
         driver.findElement(By.xpath("//*[@id=\"p_n_feature_nine_browse-bin/3291437011\"]/span/a/span")).click();
 
+
+        //Get the name of the second item from the Product List page and click it
         List<WebElement> elementList = driver.findElements(By.className("rush-component"));
         if (elementList.size() >= 2) {
 
